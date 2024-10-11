81f16eeb0283cbb0b6a2fb6c10580fae2772b67b
JerryBao02 <167569824+JerryBao02@users.noreply.github.com>
2024-10-11 00:03:18 -0400

    Added backend files

---
+++ src/main/java/com/zentravel/vacation_web/VacationWebApplication.java
@@ -0,0 +1,13 @@
+package com.zentravel.vacation_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zentravel/vacation_web/VacationWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zentravel.vacation_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
