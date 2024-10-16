b9603a296204567ff55358097fa9485e29b373e7
JerryBao02 <167569824+JerryBao02@users.noreply.github.com>
2024-10-16 00:58:24 -0400

    small update

--- src/main/java/com/zentravel/vacation_web/entity/HotelBooking.java
+++ src/main/java/com/zentravel/vacation_web/entity/HotelBooking.java
@@ -1,6 +1,7 @@
 package com.zentravel.vacation_web.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -17,6 +18,7 @@ public class HotelBooking {
     private LocalDate checkInDate;
     private LocalDate checkOutDate;
 
+    @Min(value = 1, message = "Number of Adults must be at least 1")
     private int numOfAdults;
     private int numOfKids;
     private int totalGuest;
